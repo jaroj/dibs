00503c41c067a71f3737c19481d13991a6cbf382
Robin Wunderlin <robin@vivino.com>
2016-08-03 23:08:05 +0200

    Add DA translations

--- lib/results/authorize.rb
+++ lib/results/authorize.rb
@@ -3,25 +3,40 @@ module Dibs
   module Results
     class Authorize < DibsResult
 
-      def result_text
+      def result_text(locale = :da)
         return if accepted?
-        {
-          "0"  => "Rejected by acquirer.",
-          "1"  => "Communication problems.",
-          "2"  => "Error in the parameters sent to the DIBS server. An additional parameter called \"message\" is returned, with a value that may help identifying the error.",
-          "3"  => "Error at the acquirer.",
-          "4"  => "Credit card expired.",
-          "5"  => "Your shop does not support this credit card type, the credit card type could not be identified, or the credit card number was not modulus correct.",
-          "6"  => "Instant capture failed.",
-          "7"  => "The order number (orderid) is not unique.",
-          "8"  => "There number of amount parameters does not correspond to the number given in the split parameter.",
-          "9"  => "Control numbers (cvc) are missing.",
-          "10" => "The credit card does not comply with the credit card type.",
-          "11" => "Declined by DIBS Defender.",
-          "20" => "Cancelled by user at 3D Secure authentication step"
+        text = {
+          "0"  => { en: "Rejected by acquirer.",
+                    da: "Afvist af kreditkort-udbyder" },
+          "1"  => { en: "Communication problems.",
+                    da: "Kommunikationsproblemer" },
+          "2"  => { en: "Error in the parameters sent to the DIBS server. An additional parameter called \"message\" is returned, with a value that may help identifying the error.",
+                    da: "Der er fejl i de parametre, som er sendt til DIBS serveren. Et ekstra parameter kaldet \"message\" er sendt retur, med en værdi som kan hjælpe med at identificere fejlen" },
+          "3"  => { en: "Error at the acquirer.",
+                    da: "Fejl hos kreditkortudbydere" },
+          "4"  => { en: "Credit card expired.",
+                    da: "Kreditkort er udløbet" },
+          "5"  => { en: "Your shop does not support this credit card type, the credit card type could not be identified, or the credit card number was not modulus correct.",
+                    da: "Fejl på grund af at dit køb ikke er supporteret af denne kreditkort-type, eller fordi kreditkortet skal identificeres eller fordi kreditkortnummeret var ikke korrekt" },
+          "6"  => { en: "Instant capture failed.",
+                    da: "Instant capture failed." },
+          "7"  => { en: "The order number (orderid) is not unique.",
+                    da: "Ordrenummeret er ikke unikt." },
+          "8"  => { en: "There number of amount parameters does not correspond to the number given in the split parameter.",
+                    da: "There number of amount parameters does not correspond to the number given in the split parameter." },
+          "9"  => { en: "Control numbers (cvc) are missing.",
+                    da: "Kontrolnumre (cvc) mangler at blive udfyldt" },
+          "10" => { en: "The credit card does not comply with the credit card type.",
+                    da: "The credit card does not comply with the credit card type." },
+          "11" => { en: "Declined by DIBS Defender.",
+                    da: "Afvist af DIBS Defender" },
+          "20" => { en: "Cancelled by user at 3D Secure authentication step",
+                    da: "Annulleret af bruger ved 3D Secure authentication step" },
         }[self.reason]
+        (text || {})[locale]
       end
 
     end
   end
 end
+
--- lib/results/capture.rb
+++ lib/results/capture.rb
@@ -3,25 +3,41 @@ module Dibs
   module Results
     class Capture < DibsResult
 
-      def result_text
+      def result_text(locale = :da)
         return if accepted?
-        {
-          "0"  => "Accepted",
-          "1"  => "No response from acquirer.",
-          "2"  => "Timeout",
-          "3"  => "Credit card expired.",
-          "4"  => "Rejected by acquirer.",
-          "5"  => "Authorisation older than 7 days. ",
-          "6"  => "Transaction status on the DIBS server does not allow capture.",
-          "7"  => "Amount too high.",
-          "8"  => "Error in the parameters sent to the DIBS server. An additional parameter called \"message\" is returned, with a value that may help identifying the error.",
-          "9"  => "Order number (orderid) does not correspond to the authorisation order number.",
-          "10" => "Re-authorisation of the transaction was rejected.",
-          "11" => "Not able to communicate with the acquier.",
-          "12" => "Confirm request error",
-          "14" => "Capture is called for a transaction which is pending for batch - i.e. capture was already called",
-          "15" => "Capture was blocked by DIBS."
+        text = {
+          "0"  => { en: "Accepted",
+                    da: "Accepteret" },
+          "1"  => { en: "No response from acquirer.",
+                    da: "Ingen respons fra kreditkortudbyder" },
+          "2"  => { en: "Timeout",
+                    da: "Timeout" },
+          "3"  => { en: "Credit card expired.",
+                    da: "Kreditkort er udløbet" },
+          "4"  => { en: "Rejected by acquirer.",
+                    da: "Afvist af kreditkortudbyder" },
+          "5"  => { en: "Authorisation older than 7 days. ",
+                    da: "Autorisation ældre end 7 dage." },
+          "6"  => { en: "Transaction status on the DIBS server does not allow capture.",
+                    da: "Transaction status on the DIBS server does not allow capture." },
+          "7"  => { en: "Amount too high.",
+                    da: "Beløb er for højt" },
+          "8"  => { en: "Error in the parameters sent to the DIBS server. An additional parameter called \"message\" is returned, with a value that may help identifying the error.",
+                    da: "Der er fejl i de parametre, som er sendt til DIBS serveren. Et ekstra parameter kaldet \"message\" er sendt retur, med en værdi som kan hjælpe med at identificere fejlen" },
+          "9"  => { en: "Order number (orderid) does not correspond to the authorisation order number.",
+                    da: "Order number (orderid) does not correspond to the authorisation order number." },
+          "10" => { en: "Re-authorisation of the transaction was rejected.",
+                    da: "Gen-autorisation af denne transaktion blev afvist" },
+          "11" => { en: "Not able to communicate with the acquier.",
+                    da: "Ikke muligt at kommunikere med kreditkortudbyder" },
+          "12" => { en: "Confirm request error",
+                    da: "Confirm request error" },
+          "14" => { en: "Capture is called for a transaction which is pending for batch - i.e. capture was already called",
+                    da: "Capture is called for a transaction which is pending for batch - i.e. capture was already called" },
+          "15" => { en: "Capture was blocked by DIBS.",
+                    da: "Capture was blocked by DIBS." },
         }[self.reason]
+        (text || {})[locale]
       end
 
     end
